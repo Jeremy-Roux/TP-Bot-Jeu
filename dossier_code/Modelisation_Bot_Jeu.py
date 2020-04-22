c754ad5703d41b70baf3637f0ede9529fe4997e5
Jeremy-Roux <jeremy.roux.97@gmail.com>
2020-04-22 11:51:15 +0200

    modifications

---
+++ dossier_code/Modelisation_Bot_Jeu.py
@@ -0,0 +1,9 @@
+class Game(object):
+
+    def __int__(self):
+        print("Jouez au jeu des batonnets")
+
+    def Rules(self):
+        print("Le jeu est composé de 21 batonnets, l'objectif est de ne pas tirer le dernier.")
+        print("Chaque joueur doit tirer à tour de rôle, 1, 2 ou 3 batonnets.")
+
